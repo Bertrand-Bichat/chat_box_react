19b9e7afe79677aa784aacb70b0b66daae72ee4d
Bertrand-Bichat <bertrand.bichat@gmail.com>
2020-07-09 10:43:06 +0200

    add form connexion

--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -3,8 +3,14 @@ import React, { Component } from 'react'
 class Connexion extends Component {
     render() {
         return (
-            <div>
-
+            <div className='connexionBox'>
+                <form className='connexion'>
+                    <input 
+                    placeholder = 'Pseudo'
+                    type='text'
+                    required />
+                    <button type='submit'>GO</button>
+                </form>
             </div>
         )
     }
